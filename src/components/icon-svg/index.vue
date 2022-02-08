22c037ebc5ac8b1967c91bf0cf74b1cb7b056ec5
wanglaibin <196903849@qq.com>
2022-02-08 23:34:43 +0800

    feat(svg icon): svg icon
    svg icon

---
+++ src/components/icon-svg/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg class="cicada__icon" aria-hidden="true">
+    <use :xlink:href="`#icon-${iName}`"></use>
+  </svg>
+</template>
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+  name: 'IconSvg',
+  props: {
+    name: {
+      type: String,
+      default: 'cicada'
+    }
+  },
+  setup(props) {
+    const iName = computed(() => {
+      return props.name || 'cicada'
+    })
+
+    return {
+      iName
+    }
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+.cicada__icon {
+  width: 2em;
+  height: 2em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
